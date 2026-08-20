914e158b7c730652fbacff20aa26d34bc7442086
Emiliano García <ashraahx@gmail.com>
2026-08-20 16:02:45 -0600

    Load nonverbal reaction styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './styles.css';
+import './nonverbal.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
